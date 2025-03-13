c23147cfaa491debfe7ef3ae3ed28486a7b4f840
Khent Desierto <khentjaydesierto983@gmail.com>
2025-03-14 00:47:51 +0800

    can now view group list

--- app/user/groups/page.tsx
+++ app/user/groups/page.tsx
@@ -1,6 +1,7 @@
 import { AppSidebar } from '@/components/app-sidebar'
 import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 import React from 'react'
+import { GroupList } from './group-list'
 
 const page = () => {
   return (
@@ -8,7 +9,10 @@ const page = () => {
         <AppSidebar />
         <SidebarInset>
         <SidebarTrigger className="-ml-1" />
-          <div>page</div>
+        <div className="p-4">
+          <h1 className="text-2xl font-bold mb-4">Groups</h1>
+          <GroupList /> {/* Render the GroupList component */}
+        </div>
         </SidebarInset>
     </SidebarProvider>
   )
